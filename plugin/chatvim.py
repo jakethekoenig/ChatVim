848f91e2e874d8747c699905ab0f56c1f303edb2
Jake Koenig <jakethekoenig@gmail.com>
2023-08-14 15:12:19 -0700

    GPT streaming output instead of waiting for full response.

--- plugin/chatvim.py
+++ plugin/chatvim.py
@@ -4,37 +4,76 @@ import pynvim
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def line_diff(subseq, line):
+    ans = ""
+    at = 0
+    for c in line:
+        if len(subseq) > at and c == subseq[at]:
+            at += 1
+        else:
+            ans += c
+    return ans
+
 @pynvim.plugin
 class GPTPlugin:
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @pynvim.function("GPTResponse", sync=False)
+    @pynvim.function("GPTResponse")
     def gpt_response(self, args):
-        buffer = self.nvim.current.buffer
-        row, col = self.nvim.current.window.cursor
         history, last_talked = self._get_chat_history()
+
         model = self.nvim.vars.get("gpt_model", "gpt-3.5-turbo")
-        if len(history) > 0:
-            self.nvim.loop.create_task(self.make_request_async_then_fill(history, last_talked, model, row, buffer))
-
-    async def make_request_async_then_fill(self, history, last_talked, model, row, buffer):
-        response = await self._get_gpt_response(history, last_talked, model)
-        self._insert_response(response, row, buffer)
-
-    async def _get_gpt_response(self, history, last_talked, model):
         # last_talked overrides global variable
         if last_talked == "3":
             model = "gpt-3.5-turbo"
         elif last_talked == "4":
             model = "gpt-4"
 
-        result = openai.ChatCompletion.create(
+        if len(history) > 0:
+            self.make_gpt_request(history, model)
+
+    def make_gpt_request(self, history, model):
+        response = openai.ChatCompletion.create(
             model=model,
-            messages=history ,
+            messages=history,
+            stream=True
         )
 
-        return result['choices'][0]['message']['content']
+        initial_paste_value = self.nvim.command_output('set paste?')
+        self.nvim.command('set paste')
+        self.nvim.feedkeys("oGPT: ")
+
+        total_response = ""
+        interrupted = False
+        try:
+            for chunk in response:
+                current_line = self.nvim.call('getline', '.')
+                prefix = ""
+                if current_line.startswith("GPT: "):
+                    prefix = "GPT: "
+                    current_line = current_line[5:]
+                if len(current_line) != 0 and current_line != total_response[-len(current_line):]:
+                    last_line_response = total_response.split("\n")[-1]
+                    diff = line_diff(last_line_response, current_line)
+                    self.nvim.feedkeys("\x03cc{}{}\n> {}\x03".format(prefix, last_line_response, diff))
+
+                    interrupted = True
+                    break
+                delta = chunk['choices'][0]['delta']
+                if 'content' in delta:
+                    chunk = delta['content']
+                    self.nvim.feedkeys(chunk)
+                    total_response += chunk
+        except KeyboardInterrupt:
+            self.nvim.command('echom "Keyboard Interrupt received"')
+        finally:
+            response.close()
+            if not interrupted:
+                self.nvim.feedkeys("\x03o> \x03")
+            self.nvim.command('set {}'.format(initial_paste_value))
+            if interrupted:
+                self.nvim.feedkeys("a")
 
     def _get_chat_history(self):
         cursor_line, _ = self.nvim.current.window.cursor
@@ -57,12 +96,7 @@ class GPTPlugin:
                     last_talked = "3"
                 elif line.startswith("4"):
                     last_talked = "4"
-                else:
-                    last_talked = None
             else:
                 if len(history) > 0:
                     history[-1]["content"] += "\n" + line.strip()
         return history, last_talked
-
-    def _insert_response(self, response, row, buffer):
-        buffer.append("GPT: {}".format(response), index=row)
